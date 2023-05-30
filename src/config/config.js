5c90a527e8dfeb81da1ad56a79e3f34e37175e2b
vitomarcode <marcodevito47@gmail.com>
2023-05-30 03:10:37 +0200

    https

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  baseUrl: 'http://us-central1-test-b7665.cloudfunctions.net/api/',
+  baseUrl: 'https://us-central1-test-b7665.cloudfunctions.net/api/',
   idStore: 'ijpxNJLM732vm8AeajMR'
 }
 
